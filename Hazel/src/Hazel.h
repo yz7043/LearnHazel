06a241295df6b61dfa75d4f54bca17ba8debcfc3
yz7043 <90709617+yz7043@users.noreply.github.com>
2023-02-14 15:43:20 -0500

    finish log and premake

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,7 +3,7 @@
 // For hazel application use only
 #include <stdio.h>
 #include "Hazel/Application.h"
-
+#include "Hazel/Log.h"
 // ------------------- Entry Point -------------------
 #include "Hazel/EntryPoint.h"
 // ------------------- Entry Point -------------------
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,9 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int arc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Initialized Log!");
+	HZ_INFO("Hello!");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;

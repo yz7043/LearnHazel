479fc05137086b0772edf70406d422adf182a5b4
yz7043 <90709617+yz7043@users.noreply.github.com>
2023-02-14 02:44:04 -0500

    Setup basic Entry point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For hazel application use only
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// ------------------- Entry Point -------------------
+#include "Hazel/EntryPoint.h"
+// ------------------- Entry Point -------------------
